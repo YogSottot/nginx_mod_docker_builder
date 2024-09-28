d4efd1c38106ac6812dc57794cb6f3bb8562ba04
YogSottot <7411302+YogSottot@users.noreply.github.com>
2024-09-28 15:53:20 +0600

    fix *

--- debian-12.sh
+++ debian-12.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 mkdir -p module_$(basename "$0")
 docker build --tag nginx_mod_docker_build --file Debian-12 . &&  \
 id=$(docker create nginx_mod_docker_build)
-docker cp $id:/usr/local/nginx/modules/*.so module_$(basename "$0")/ && \
+docker cp $id:/usr/local/nginx/modules/ module_$(basename "$0")/ && \
 docker rm -v $id && \
 docker image rm nginx_mod_docker_build
