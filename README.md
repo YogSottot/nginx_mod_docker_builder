262960f9d74e7cd1618865edfc80910523b9e10a
YogSottot <7411302+YogSottot@users.noreply.github.com>
2025-08-12 20:04:56 +0300

    Add trixie

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ To build, run ```debian-12.sh``` or ```ubuntu-24.04.sh``` script.
 Modules will be built for standard versions of nginx from the distributions own repositories.  
 
 For ```debian-12``` — ```nginx 1.22.1```  
+For ```debian-13``` — ```nginx 1.26.3```  
 For ```ubuntu 24.04``` — ```nginx 1.24.0```  
 
 The result of building is placed in the directory ```fpm/${mod_name}/${os_version}```  
@@ -15,3 +16,8 @@ The result of building is placed in the directory ```fpm/${mod_name}/${os_versio
 
 Copy the module to the server in the ```/etc/nginx/modules-available/``` directory and place the ```50-mod-*.conf``` file in ```/etc/nginx/modules-enabled```
 or install deb packages from ```fpm/deb``` or setup repo with this packages https://yogsottot.github.io/ppa/  
+
+For building you need fpm  
+```
+gem install --no-document --user-install fpm
+```
---
+++ debian-13.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+mkdir -p module_$(basename "$0")
+mkdir -p fpm/deb/trixie
+docker build --tag nginx_mod_docker_build --file Debian-13 . &&  \
+id=$(docker create nginx_mod_docker_build)
+docker cp "${id}":/usr/local/nginx/modules/ module_$(basename "$0")/ && \
+# zip
+docker cp "${id}":/usr/local/nginx/modules/ngx_http_zip_module.so fpm/mod_zip/trixie/usr/lib/nginx/modules/ && \
+# ntlm
+docker cp "${id}":/usr/local/nginx/modules/ngx_http_upstream_ntlm_module.so fpm/mod_ntlm/trixie/usr/lib/nginx/modules/ && \
+# zstd
+docker cp "${id}":/usr/local/nginx/modules/ngx_http_zstd_filter_module.so fpm/mod_zstd/trixie/usr/lib/nginx/modules/ && \
+docker cp "${id}":/usr/local/nginx/modules/ngx_http_zstd_static_module.so fpm/mod_zstd/trixie/usr/lib/nginx/modules/ && \
+# delete image and container
+docker rm -v "${id}" && \
+
+# build libnginx-mod-http-zip deb package
+pushd fpm/mod_zip/trixie/ && \
+fpm \
+  -s dir -t deb \
+  -p libnginx-mod-http-zip_1.3.0-1.gbp8e65b82_trixie_amd64.deb \
+  --name libnginx-mod-http-zip \
+  --license bsd3 \
+  --version 1.3.0-1.gbp8e65b82~trixie \
+  --architecture amd64 \
+  --depends "nginx-abi-1.26.3-3, libc6 (>= 2.4)" \
+  --description "mod_zip assembles ZIP archives dynamically. It can stream component files from upstream servers with nginx's native proxying code, so that the process never takes up more than a few KB of RAM at a time, even while assembling archives that are (potentially) gigabytes in size." \
+  --url "https://github.com/evanmiller/mod_zip" \
+  --maintainer "YogSottot  <7411302+YogSottot@users.noreply.github.com>" \
+  --deb-changelog "debian/changelog.Debian" \
+  --category httpd \
+  --after-remove debian/postrm \
+  --after-install debian/postinst \
+  --before-remove debian/prerm \
+  --package ../../deb/trixie/ \
+  usr/ && \
+popd && \
+# build libnginx-mod-http-ntlm deb package
+pushd fpm/mod_ntlm/trixie/ && \
+fpm \
+  -s dir -t deb \
+  -p libnginx-mod-http-ntlm_1.19.3-1.gbp3da77b0_trixie_amd64.deb \
+  --name libnginx-mod-http-ntlm \
+  --license bsd3 \
+  --version 1.19.3-1.gbp3da77b0~trixie \
+  --architecture amd64 \
+  --depends "nginx-abi-1.26.3-3, libc6 (>= 2.4)" \
+  --description "The NTLM module allows proxying requests with NTLM Authentication. The upstream connection is bound to the client connection once the client sends a request with the "Authorization" header field value starting with "Negotiate" or "NTLM". Further client requests will be proxied through the same upstream connection, keeping the authentication context." \
+  --maintainer "YogSottot  <7411302+YogSottot@users.noreply.github.com>" \
+  --url "https://github.com/gabihodoroaga/nginx-ntlm-module" \
+  --deb-changelog "debian/changelog.Debian" \
+  --category httpd \
+  --after-remove debian/postrm \
+  --after-install debian/postinst \
+  --before-remove debian/prerm \
+  --package ../../deb/trixie/ \
+  usr/ && \
+popd && \
+# build libnginx-mod-http-zstd deb package
+pushd fpm/mod_zstd/trixie/ && \
+fpm \
+  -s dir -t deb \
+  -p libnginx-mod-http-zstd_0.1.1-1.gbp057a7d3_trixie_amd64.deb \
+  --name libnginx-mod-http-zstd \
+  --license bsd3 \
+  --version 0.1.1-1.gbp057a7d3~trixie \
+  --architecture amd64 \
+  --depends "nginx-abi-1.26.3-3, libc6 (>= 2.4)" \
+  --description "NGINX module for ZSTD compression" \
+  --maintainer "YogSottot  <7411302+YogSottot@users.noreply.github.com>" \
+  --url "https://github.com/tokers/zstd-nginx-module" \
+  --deb-changelog "debian/changelog.Debian" \
+  --category httpd \
+  --after-remove debian/postrm \
+  --after-install debian/postinst \
+  --before-remove debian/prerm \
+  --package ../../deb/trixie/ \
+  usr/ && \
+popd
+
+docker image rm nginx_mod_docker_build 
