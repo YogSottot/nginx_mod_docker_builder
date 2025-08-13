f186b23d0dc1ab0344ae3a73effefa648aaa25f5
YogSottot <7411302+YogSottot@users.noreply.github.com>
2025-08-13 16:49:23 +0300

    Bump zstd trixie version

--- debian-13.sh
+++ debian-13.sh
@@ -25,7 +25,7 @@ fpm \
   --license bsd3 \
   --version 1.3.0-1.gbp8e65b82~trixie \
   --architecture amd64 \
-  --depends "nginx-abi-1.26.3-3, libc6 (>= 2.4)" \
+  --depends "nginx-abi-1.26.3-1, libc6 (>= 2.4)" \
   --description "mod_zip assembles ZIP archives dynamically. It can stream component files from upstream servers with nginx's native proxying code, so that the process never takes up more than a few KB of RAM at a time, even while assembling archives that are (potentially) gigabytes in size." \
   --url "https://github.com/evanmiller/mod_zip" \
   --maintainer "YogSottot  <7411302+YogSottot@users.noreply.github.com>" \
@@ -46,7 +46,7 @@ fpm \
   --license bsd3 \
   --version 1.19.3-1.gbp3da77b0~trixie \
   --architecture amd64 \
-  --depends "nginx-abi-1.26.3-3, libc6 (>= 2.4)" \
+  --depends "nginx-abi-1.26.3-1, libc6 (>= 2.4)" \
   --description "The NTLM module allows proxying requests with NTLM Authentication. The upstream connection is bound to the client connection once the client sends a request with the "Authorization" header field value starting with "Negotiate" or "NTLM". Further client requests will be proxied through the same upstream connection, keeping the authentication context." \
   --maintainer "YogSottot  <7411302+YogSottot@users.noreply.github.com>" \
   --url "https://github.com/gabihodoroaga/nginx-ntlm-module" \
@@ -62,12 +62,12 @@ popd && \
 pushd fpm/mod_zstd/trixie/ && \
 fpm \
   -s dir -t deb \
-  -p libnginx-mod-http-zstd_0.1.1-1.gbp057a7d3_trixie_amd64.deb \
+  -p libnginx-mod-http-zstd_0.1.1-2.gbp057a7d3_trixie_amd64.deb \
   --name libnginx-mod-http-zstd \
   --license bsd3 \
-  --version 0.1.1-1.gbp057a7d3~trixie \
+  --version 0.1.1-2.gbp057a7d3~trixie \
   --architecture amd64 \
-  --depends "nginx-abi-1.26.3-3, libc6 (>= 2.4)" \
+  --depends "nginx-abi-1.26.3-1, libc6 (>= 2.4)" \
   --description "NGINX module for ZSTD compression" \
   --maintainer "YogSottot  <7411302+YogSottot@users.noreply.github.com>" \
   --url "https://github.com/tokers/zstd-nginx-module" \
